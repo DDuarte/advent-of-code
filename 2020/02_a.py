0e6108bd4bb6e304a154c86b05d179027940721e
Duarte Duarte <dnpd.dd@gmail.com>
2020-12-02 23:12:43 +0000

    2020 day 2

---
+++ 2020/02_a.py
@@ -0,0 +1,13 @@
+lines = [line.strip().split(" ") for line in open("2_input.txt", "r").readlines()]
+
+cnt = 0
+
+for line in lines:
+    pos = line[0].split("-")
+    pos1, pos2, letter, passw = int(pos[0]), int(pos[1]), line[1].rstrip(":"), line[2]
+
+    occ = passw.count(letter)
+    if occ >= pos1 and occ <= pos2:
+        cnt += 1
+
+print(cnt)
---
+++ 2020/02_b.py
@@ -0,0 +1,12 @@
+lines = [line.strip().split(" ") for line in open("2_input.txt", "r").readlines()]
+
+cnt = 0
+
+for line in lines:
+    pos = line[0].split("-")
+    pos1, pos2, letter, passw = int(pos[0]), int(pos[1]), line[1].rstrip(":"), line[2]
+
+    if (passw[pos1 - 1] == letter) != (passw[pos2 - 1] == letter):
+        cnt += 1
+
+print(cnt)
