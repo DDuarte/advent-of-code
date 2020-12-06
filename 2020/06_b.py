1f2d4b8e07e99dde97936606e820e078ec19ea3c
Duarte Duarte <dnpd.dd@gmail.com>
2020-12-06 14:45:49 +0000

    2020 day 6 shorter

--- 2020/06_b.py
+++ 2020/06_b.py
@@ -2,7 +2,7 @@ import re
 
 print(
     sum(
-        len(set.intersection(*[set(s) for s in line.split("\n")]))
+        len(set.intersection(*map(set, line.split("\n"))))
         for line in re.split("\n\n", open("2020/06_input.txt", "r").read().strip())
     )
 )
