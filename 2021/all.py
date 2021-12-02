beff88fa71fe6f8d85a4ea3c291e35ab12b5c09d
Duarte Duarte <duarte.duarte@blip.pt>
2021-12-02 14:39:01 +0000

    2021 day 2

--- 2021/all.py
+++ 2021/all.py
@@ -1,17 +1,43 @@
-# 01A
+def aoc_01() -> tuple[int, int]:
+    m = [int(line.strip()) for line in open("2021/01_input.txt").readlines()]
 
-m = [int(line.strip()) for line in open("2021/01_input.txt").readlines()]
-result = sum(i > 0 and m[i - 1] < m[i] for i in range(len(m)))
+    def a(m: list[int]) -> int:
+        return sum(m[i - 1] < m[i] for i in range(1, len(m)))
 
-assert result == 1548
+    def b(m: list[int]) -> int:
+        return sum(m[i - 3] < m[i] for i in range(1, len(m)))
 
-print("01A:", result)
+    return a(m), b(m)
 
-# 01B
+assert aoc_01() == (1548, 1589)
 
-m = [sum(g) for g in [m[i : i + 3] for i in range(len(m) - 3 + 1)]]
-result = sum(i > 0 and m[i - 1] < m[i] for i in range(len(m)))
+def aoc_02() -> tuple[int, int]:
+    instructions = [(i, int(u)) for i, u in [line.strip().split(" ") for line in open("2021/02_input.txt").readlines()]]
 
-assert result == 1589
+    def a(instructions: list[tuple[str, int]]) -> int:
+        h = d = 0
 
-print("01B:", result)
+        for i, u in instructions:
+            match i:
+                case "forward": h += u
+                case "down": d += u
+                case "up": d -= u
+
+        return h * d
+
+    def b(instructions: list[tuple[str, int]]) -> int:
+        h = d = a = 0
+
+        for i, u in instructions:
+            match i:
+                case "forward":
+                    h += u
+                    d += a * u
+                case "down": a += u
+                case "up": a -= u
+
+        return h * d
+
+    return a(instructions), b(instructions)
+
+assert aoc_02() == (1924923, 1982495697)
