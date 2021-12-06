34185377bb4176b365067437da8c08ee0e98e433
Duarte Duarte <duarte.duarte@blip.pt>
2021-12-06 03:00:57 +0000

    2021 day 4

--- 2021/all.py
+++ 2021/all.py
@@ -1,4 +1,4 @@
-from typing import Literal
+from typing import Literal, Tuple, Union
 
 
 def aoc_01(f: str) -> tuple[int, int]:
@@ -12,8 +12,8 @@ def aoc_01(f: str) -> tuple[int, int]:
 
     return a(m), b(m)
 
-assert aoc_01("2021/01_sample.txt") == (7, 5)
-assert aoc_01("2021/01_input.txt") == (1548, 1589)
+# assert aoc_01("2021/01_sample.txt") == (7, 5)
+# assert aoc_01("2021/01_input.txt") == (1548, 1589)
 
 def aoc_02(f: str) -> tuple[int, int]:
     instructions = [(i, int(u)) for i, u in [line.strip().split(" ") for line in open(f).readlines()]]
@@ -44,33 +44,75 @@ def aoc_02(f: str) -> tuple[int, int]:
 
     return a(instructions), b(instructions)
 
-assert aoc_02("2021/02_sample.txt") == (150, 900)
-assert aoc_02("2021/02_input.txt") == (1924923, 1982495697)
+# assert aoc_02("2021/02_sample.txt") == (150, 900)
+# assert aoc_02("2021/02_input.txt") == (1924923, 1982495697)
 
 def aoc_03(f: str) -> tuple[int, int]:
     bits = [line.strip() for line in open(f).readlines()]
 
-    def most_common(bits: list[str], i) -> Literal["1", "0"]:
+    def _most_common(bits: list[str], i) -> Literal["1", "0"]:
         cnt = sum(b[i] == "1" for b in bits)
         return "1" if cnt >= len(bits)/2 else "0"
 
     def a(bits: list[str]) -> int:
-        r = "".join([most_common(bits, i) for i in range(len(bits[0]))])
-        return  int(r, 2) * int(r.translate(str.maketrans("01", "10")), 2)
+        r = "".join([_most_common(bits, i) for i in range(len(bits[0]))])
+        return int(r, 2) * int(r.translate(str.maketrans("01", "10")), 2)
 
     def b(bits: list[str]) -> int:
         bits2 = bits.copy()
 
         for i in range(len(bits[0])):
             if len(bits) > 1:
-                bits = [b for b in bits if b[i] == most_common(bits, i)]
+                bits = [b for b in bits if b[i] == _most_common(bits, i)]
 
             if len(bits2) > 1:
-                bits2 = [b for b in bits2 if b[i] != most_common(bits2, i)]
+                bits2 = [b for b in bits2 if b[i] != _most_common(bits2, i)]
 
         return int(bits[0], 2) * int(bits2[0], 2)
 
     return a(bits), b(bits)
 
-assert aoc_03("2021/03_sample.txt") == (198, 230)
-assert aoc_03("2021/03_input.txt") == (2583164, 2784375)
+# assert aoc_03("2021/03_sample.txt") == (198, 230)
+# assert aoc_03("2021/03_input.txt") == (2583164, 2784375)
+
+def aoc_04(f: str) -> tuple[int, int]:
+    with open(f) as of:
+        order = [int(n) for n in of.readline().strip().split(",")]
+
+        boards = [[[int(n), False] for n in l.strip().split(" ") if n != ""] for l in of.readlines() if l != "\n"]
+        boards = [boards[i:i + 5] for i in range(0, len(boards), 5)]
+
+    def a(order: list[int], boards: list[list[list[list[Union[int, bool]]]]]) -> int:
+        for o in order:
+            [n.__setitem__(1, True) for board in boards for line in board for n in line if o == n[0]]
+
+            for board, boardT in zip(boards, [list(map(list, zip(*board))) for board in boards]):
+                for line, col in zip(board, boardT):
+                    if all([n[1] for n in line]) or all([n[1] for n in col]):
+                        return o * sum([n[0] for line in board for n in line if not n[1]])
+
+    def b(order: list[int], boards: list[list[list[Tuple[int, bool]]]]) -> int:
+        boardswon = 0
+        boardswon2 = set()
+        win = None
+
+        for o in order:
+            [n.__setitem__(1, True) for board in boards for line in board for n in line if o == n[0]]
+
+            i = 0
+            for board, boardT in zip(boards, [list(map(list, zip(*board))) for board in boards]):
+                for line, col in zip(board, boardT):
+                    if all([n[1] for n in line]) or all([n[1] for n in col]):
+                        win = o * sum([n[0] for line in board for n in line if not n[1]])
+                        boardswon2.add(i)
+                        break
+                i += 1
+                if len(boardswon2) == len(boards):
+                    return win
+
+        return win
+
+    return a(order, boards), b(order, boards)
+
+assert aoc_04("2021/04_sample.txt") == (4512, 1924)
+assert aoc_04("2021/04_input.txt") == (50008, 17408)
