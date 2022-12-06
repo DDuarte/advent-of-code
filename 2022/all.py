a0a3ee892037b43a2c6a47b2a341166a74ff6645
Duarte Duarte <duarte.duarte@blip.pt>
2022-12-06 01:03:34 +0000

    refactor

--- 2022/all.py
+++ 2022/all.py
@@ -1,98 +1,106 @@
-def aoc_01_a(f: str) -> int:
-    highest = 0
-    curr = 0
-    for line in open(f).readlines() + [""]:
-        line = line.strip()
-        if line == "":
-            if curr > highest:
-                highest = curr
-            curr = 0
-        else:
-            curr += int(line)
+def aoc_01(f: str) -> tuple[int, int]:
+    lines = open(f).readlines() + [""]
 
-    return highest
+    def a(lines: list[str]) -> int:
+        highest = 0
+        curr = 0
+        for line in lines:
+            line = line.strip()
+            if line == "":
+                if curr > highest:
+                    highest = curr
+                curr = 0
+            else:
+                curr += int(line)
 
+        return highest
 
-def aoc_01_b(f: str) -> int:
-    sums = []
-    curr = 0
-    for line in open(f).readlines() + [""]:
-        line = line.strip()
-        if line == "":
-            sums.append(curr)
-            curr = 0
-        else:
-            curr += int(line)
+    def b(lines: list[str]) -> int:
+        sums = []
+        curr = 0
+        for line in lines:
+            line = line.strip()
+            if line == "":
+                sums.append(curr)
+                curr = 0
+            else:
+                curr += int(line)
 
-    sums.sort()
-    return sums[-3] + sums[-2] + sums[-1]
+        sums.sort()
+        return sums[-3] + sums[-2] + sums[-1]
 
-
-assert aoc_01_a("2022/01_sample.txt") == 24000
-assert aoc_01_a("2022/01_input.txt") == 69528
-assert aoc_01_b("2022/01_sample.txt") == 45000
-assert aoc_01_b("2022/01_input.txt") == 206152
+    return a(lines), b(lines)
 
 
-def aoc_02_a(f: str) -> int:
+assert aoc_01("2022/01_sample.txt") == (24000, 45000)
+assert aoc_01("2022/01_input.txt") == (69528, 206152)
+
+
+def aoc_02(f: str) -> tuple[int, int]:
+    from typing import Literal
+
     strategy = [(x, y) for x, y in [line.strip().split(" ") for line in open(f)]]
+    LOST = 0
+    DRAW = 3
+    WIN = 6
+    ROCK = 1
+    PAPER = 2
+    SCISSORS = 3
 
-    def score(strat) -> int:
-        match strat:
-            case ("A", "Y"):
-                return 2 + 6
-            case ("A", "X"):
-                return 1 + 3
-            case ("A", "Z"):
-                return 3 + 0
-            case ("B", "Y"):
-                return 2 + 3
-            case ("B", "X"):
-                return 1 + 0
-            case ("B", "Z"):
-                return 3 + 6
-            case ("C", "Z"):
-                return 3 + 3
-            case ("C", "X"):
-                return 1 + 6
-            case ("C", "Y"):
-                return 2 + 0
-            case _:
-                raise UnboundLocalError
+    def a(strategy: list[tuple[str, str]]) -> int:
+        def score(strat) -> int:
+            match strat:
+                case ("A", "Y"):
+                    return PAPER + WIN
+                case ("A", "X"):
+                    return ROCK + DRAW
+                case ("A", "Z"):
+                    return SCISSORS + LOST
+                case ("B", "Y"):
+                    return PAPER + DRAW
+                case ("B", "X"):
+                    return ROCK + LOST
+                case ("B", "Z"):
+                    return SCISSORS + WIN
+                case ("C", "Z"):
+                    return SCISSORS + DRAW
+                case ("C", "X"):
+                    return ROCK + WIN
+                case ("C", "Y"):
+                    return PAPER + LOST
+                case _:
+                    raise UnboundLocalError
 
-    return sum(map(score, strategy))
+        return sum(map(score, strategy))
+
+    def b(strategy: list[tuple[str, str]]) -> int:
+        def score(strat) -> int:
+            match strat:
+                case ("A", "X"):
+                    return LOST + SCISSORS
+                case ("A", "Y"):
+                    return DRAW + ROCK
+                case ("A", "Z"):
+                    return WIN + PAPER
+                case ("B", "X"):
+                    return LOST + ROCK
+                case ("B", "Y"):
+                    return DRAW + PAPER
+                case ("B", "Z"):
+                    return WIN + SCISSORS
+                case ("C", "X"):
+                    return LOST + PAPER
+                case ("C", "Y"):
+                    return DRAW + SCISSORS
+                case ("C", "Z"):
+                    return WIN + ROCK
+                case _:
+                    raise UnboundLocalError
+
+        return sum(map(score, strategy))
+
+    return a(strategy), b(strategy)
 
 
-def aoc_02_b(f: str) -> int:
-    strategy = [(x, y) for x, y in [line.strip().split(" ") for line in open(f)]]
-
-    def score(strat) -> int:
-        match strat:
-            case ("A", "X"):
-                return 0 + 3
-            case ("A", "Y"):
-                return 3 + 1
-            case ("A", "Z"):
-                return 6 + 2
-            case ("B", "X"):
-                return 0 + 1
-            case ("B", "Y"):
-                return 3 + 2
-            case ("B", "Z"):
-                return 6 + 3
-            case ("C", "X"):
-                return 0 + 2
-            case ("C", "Y"):
-                return 3 + 3
-            case ("C", "Z"):
-                return 6 + 1
-            case _:
-                raise UnboundLocalError
-
-    return sum(map(score, strategy))
-
-
-assert aoc_02_a("2022/02_sample.txt") == 15
-assert aoc_02_a("2022/02_input.txt") == 10816
-assert aoc_02_b("2022/02_sample.txt") == 12
-assert aoc_02_b("2022/02_input.txt") == 11657
+assert aoc_02("2022/02_sample.txt") == (15, 12)
+assert aoc_02("2022/02_input.txt") == (10816, 11657)
